fb5e8352ed3729df921b668e1407dce8fcade8ae
CharlesCharlotte <zcr13@mails.tsinghua.edu.cn>
2016-02-06 10:02:56 +0800

    third commit

--- ReadMusic.m
+++ ReadMusic.m
@@ -1,15 +1,14 @@
 function [trainSet,testSet,fs]=ReadMusic()
 %--Read music files,results storing in trainSet for training and testSet for testing
 %--For each genre,data is randomly split into two parts,70% for training and 30% for testing
-	[~,fs]=audioread('Various Artists - Deck The Halls.mp3',[1 100]); %读取一首歌曲得到采样频率
-	first=fs*60;
-	last=first+fs*10; %将读取乐曲第60~70秒的部分
+    first=0;last=0;
     filepath='F:\学习\大三上\随机过程\第二次Project\Data\sample'; 
     genre=dir(filepath);
     genreNum=size(genre,1)-2; %音乐曲风类别数目
     genreName=cell(genreNum,1); %曲风名称
     trainSet=cell(genreNum,1);
     testSet=cell(genreNum,1);
+    fs=cell(genreNum,1);
     for k=1:genreNum
         genreName{k}=genre(k+2).name;
     end
@@ -21,11 +20,17 @@ function [trainSet,testSet,fs]=ReadMusic()
         music_path_list= dir(strcat(genrepath,'*.mp3'));
         %%%modified
         music_num(k)=min(30,length(music_path_list));
+        fscache=zeros(1,music_num(k));
 		piece=zeros(last-first,music_num(k)); %每一列对应一首乐曲
 		for m=1:music_num(k)
+            info=audioinfo([genrepath,music_path_list(m).name]);
+            fscache(m)=info.SampleRate;
+            first=fscache(m)*floor(info.Duration/2);
+            last=min(first+10*fscache(m),info.TotalSamples);
 			piece(:,m)=mean(audioread([genrepath,music_path_list(m).name],[first+1,last]),2);%取多声道平均值作为单声道的值 
             m
         end
+        fs{k}=fscache;
 		piece=piece-repmat(mean(piece),last-first,1); %减直流将时域数据的均值调成0
 		piece=piece./repmat(sqrt(var(piece)),last-first,1); %将方差调整为1
         train_num(k)=round(0.7*music_num(k));
--- TopModule.m
+++ TopModule.m
@@ -11,14 +11,15 @@ addpath(genpath('F:\学习\大三上\随机过程\第二次Project\codes\HMMall'));
 % Training
 for k=1:genreNum
     pieces=trainSet{k}; %每一列对应一个音乐片段
+    fsmat=fs{k}; %音乐片段的采样率
     for m=1:train_num(k)
         if(m==1)
-            [r0,delta_r0,delta_delta_r0]=MFCC(pieces(:,m),fs);
+            [r0,delta_r0,delta_delta_r0]=MFCC(pieces(:,m),fsmat(m));
             [x,y]=size(r0);
             r1=zeros(3*x,y,train_num(k));
             r1(:,:,1)=[r0;delta_r0;delta_delta_r0];
         else
-            [r1(1:x,:,m),r1(x+1:2*x,:,m),r1(2*x+1:3*x,:,m)]=MFCC(pieces(:,m),fs);
+            [r1(1:x,:,m),r1(x+1:2*x,:,m),r1(2*x+1:3*x,:,m)]=MFCC(pieces(:,m),fsmat(m));
         end
         m
     end
@@ -36,8 +37,9 @@ num=cell(genreNum,1);%每个测试片段的分类结果
 for k=1:genreNum
     testset=testSet{k};
     num0=zeros(test_num(k),1);
+    fsmat=fs{k};
     for m=1:test_num(k)
-        [r,delta_r,delta_delta_r]=MFCC(testset(:,m),fs);
+        [r,delta_r,delta_delta_r]=MFCC(testset(:,m),fsmat(m));
         testdata=[r;delta_r;delta_delta_r];
         loglik=zeros(genreNum,1);
         for n=1:genreNum
